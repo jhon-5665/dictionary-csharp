5620d6b45966b4ca70a78f6d57e19c1e347ba4cd
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2020-11-26 10:29:04 -0300

    Projeto finalizado

--- dictionary-csharp/Program.cs
+++ dictionary-csharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Course
 {
@@ -7,19 +8,38 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> candidates = new Dictionary<string, int>();
+
             Console.Write("Enter file full path: ");
             string path = Console.ReadLine();
+
             try
             {
                 using (StreamReader sr = File.OpenText(path))
                 {
                     while (!sr.EndOfStream)
                     {
-                        string line = sr.ReadLine();
-                        Console.WriteLine(line);
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        
+                        if (candidates.ContainsKey(name))
+                        {
+                            candidates[name] += votes;
+                        }
+                        else
+                        {
+                            candidates[name] = votes;
+                        }
+                    }
+
+                    foreach (KeyValuePair<string, int> item in candidates)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
                     }
                 }
             }
+
             catch (IOException e)
             {
                 Console.WriteLine(e.Message);
